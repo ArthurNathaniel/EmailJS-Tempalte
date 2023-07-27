4097db7c5fe4e328f79eb8ba938e397477f60447
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-07-27 04:09:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 
+ var swiper = new Swiper(".mySwiper", {
+            autoplay: true,
+            loop: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            }
+        });
+
+        (function () {
+            emailjs.init("V25YH8woz5YG7ubmw");
+        })();
+
+        document.getElementById('contact-form').addEventListener('submit', function (event) {
+            event.preventDefault();
+            sendMail();
+        });
+
+        function sendMail() {
+            var params = {
+                name: document.getElementById('name').value,
+                phone: document.getElementById('phone').value,
+                message: document.getElementById('message').value
+            };
+
+            const serviceID = 'service_yoi94cf';
+            const templateID = 'template_953dmf6';
+
+            emailjs.send(serviceID, templateID, params)
+                .then(function (response) {
+                    console.log('Email sent successfully:', response);
+                    alert('Your message has been sent! We will get back to you soon.');
+                }, function (error) {
+                    console.error('Email failed to send:', error);
+                    alert('Sorry, there was an error sending your message. Please try again later.');
+                });
+        }
